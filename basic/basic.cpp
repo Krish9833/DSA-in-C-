47d4172c0c580323cae19a4f0ba3c52ad9cb6656
Krish9833 <129373586+Krish9833@users.noreply.github.com>
2024-07-28 16:24:32 +0000

    basic

---
+++ basic/basic.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// int x;
+// cin >> x ;
+// cout << x ;
+//   cout << "namaste cpp!";
+
+// long  x = 15;
+// cin >> x;
+// cout << x;
+
+// long long x = 15000;
+// cout << x <<"\n";
+
+// float - 4 byte
+// float y = 7.564 ;
+// cout << y ;
+
+//  double - 8 bytes
+// double myNum = 19.995956231;
+// cout << myNum;
+
+//  string and get line
+// string s1;
+// string s2;
+// cin >> s1 >> s2;
+// cout << s1 << "" << s2 ;
+
+// using getline
+string str;
+getline (cin,str); //it prints whole line ,don't break
+cout << str;
+  return 0;
+}
+
